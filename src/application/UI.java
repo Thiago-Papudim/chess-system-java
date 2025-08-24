78d6960da2dafd9c52b142db177fe7ab66ad4e09
Thiago Soares <thiago_soares_mago@hotmail.com>
2025-08-24 10:13:06 -0300

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -32,10 +32,10 @@ public class UI {
 
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
 	public static void clearScreen() {
-		
+
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
-		
+
 	}
 
 	public static ChessPosition readChessPosition(Scanner sc) {
@@ -65,7 +65,7 @@ public class UI {
 
 			for (int j = 0; j < pieces.length; j++) {
 
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
 
 			}
 
@@ -77,11 +77,37 @@ public class UI {
 
 	}
 
-	private static void printPiece(ChessPiece piece) {
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+
+		for (int i = 0; i < pieces.length; i++) {
+
+			System.out.print((8 - i) + " ");
+
+			for (int j = 0; j < pieces.length; j++) {
+
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+
+			}
+
+			System.out.println();
+
+		}
+
+		System.out.println("  a b c d e f g h");
+
+	}
+
+	private static void printPiece(ChessPiece piece, boolean background) {
+		
+		if(background) {
+			
+			System.out.print(ANSI_BLUE_BACKGROUND);
+			
+		}
 
 		if (piece == null) {
 
-			System.out.print("-");
+			System.out.print("-" + ANSI_RESET);
 
 		}
 
