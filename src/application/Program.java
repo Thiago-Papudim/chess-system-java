7a1fb196d94625841d2e152f32c555eeffd8b492
Thiago Soares <thiago_soares_mago@hotmail.com>
2025-09-15 11:26:00 -0300

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,79 +12,87 @@ import chess.ChessPosition;
 
 public class Program {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        ChessMatch chessMatch = new ChessMatch();
-        List<ChessPiece> captured = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 
-        while (!chessMatch.getCheckMate()) {
-            try {
-                UI.clearScreen();
-                UI.printMatch(chessMatch, captured);
-                System.out.println();
+		while (!chessMatch.getCheckMate()) {
+			try {
+				UI.clearScreen();
+				UI.printMatch(chessMatch, captured);
+				System.out.println();
 
-                ChessPosition source = null;
-                while (source == null) {
-                    System.out.print("Source: ");
-                    if (!sc.hasNextLine()) {
-                        System.out.println("No input found. Exiting...");
-                        sc.close();
-                        return;
-                    }
-                    try {
-                        source = UI.readChessPosition(sc);
-                    } catch (ChessException | InputMismatchException e) {
-                        System.out.println("Error: " + e.getMessage());
-                        source = null;
-                    }
-                }
+				ChessPosition source = null;
+				while (source == null) {
+					System.out.print("Source: ");
+					if (!sc.hasNextLine()) {
+						System.out.println("No input found. Exiting...");
+						sc.close();
+						return;
+					}
+					try {
+						source = UI.readChessPosition(sc);
+					} catch (ChessException | InputMismatchException e) {
+						System.out.println("Error: " + e.getMessage());
+						source = null;
+					}
+				}
 
-                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-                UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces(), possibleMoves);
-                System.out.println();
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
 
-                ChessPosition target = null;
-                while (target == null) {
-                    System.out.print("Target: ");
-                    if (!sc.hasNextLine()) {
-                        System.out.println("No input found. Exiting...");
-                        sc.close();
-                        return;
-                    }
-                    try {
-                        target = UI.readChessPosition(sc);
-                    } catch (ChessException | InputMismatchException e) {
-                        System.out.println("Error: " + e.getMessage());
-                        target = null;
-                    }
-                }
+				ChessPosition target = null;
+				while (target == null) {
+					System.out.print("Target: ");
+					if (!sc.hasNextLine()) {
+						System.out.println("No input found. Exiting...");
+						sc.close();
+						return;
+					}
+					try {
+						target = UI.readChessPosition(sc);
+					} catch (ChessException | InputMismatchException e) {
+						System.out.println("Error: " + e.getMessage());
+						target = null;
+					}
+				}
 
-                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-                if (capturedPiece != null) {
-                    captured.add(capturedPiece);
-                }
-                
-                if (chessMatch.getPromoted() != null) {
-                	
-                	System.out.print("Enter piece for promotion (B/N/R/Q): ");
-                	String type = sc.nextLine();
-                	chessMatch.replacePromotedPiece(type);
-                	
-                }
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 
-            } catch (ChessException e) {
-                System.out.println("Chess error: " + e.getMessage());
-                sc.nextLine();
-            } catch (InputMismatchException e) {
-                System.out.println("Input error: " + e.getMessage());
-                sc.nextLine();
-            }
-        }
+				if (chessMatch.getPromoted() != null) {
 
-        UI.clearScreen();
-        UI.printMatch(chessMatch, captured);
-        sc.close();
-    }
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine().toUpperCase();
+
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+
+					}
+
+					chessMatch.replacePromotedPiece(type);
+
+				}
+
+			} catch (ChessException e) {
+				System.out.println("Chess error: " + e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println("Input error: " + e.getMessage());
+				sc.nextLine();
+			}
+		}
+
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
+		sc.close();
+	}
 }
